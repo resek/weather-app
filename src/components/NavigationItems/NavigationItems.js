ceadbfeb7e7d82eb8292415fe2b5043a0b1ad593
Resek <matija.resek@gmail.com>
2018-09-18 12:56:42 +0200

    responsive ready

--- src/components/NavigationItems/NavigationItems.js
+++ src/components/NavigationItems/NavigationItems.js
@@ -1,28 +1,18 @@
-import React, {Fragment} from "react";
+import React from "react";
 import {NavLink} from "react-router-dom";
 import classes from "./NavigationItems.css";
-
-const firstDiv = {
-    justifySelf: "right",
-    alignSelf: "center"
-};
-
-const secondDiv = {
-    justifySelf: "left",
-    alignSelf: "center"
-};
 
 const NavigationItems = () => {
 
     return (
-        <Fragment>
-            <div style={firstDiv} className={classes.NavigationItems}>
+        <div className={classes.ButtonsDiv}>
+            <div className={classes.NavigationItems}>
                 <NavLink to="/" exact activeClassName={classes.active}>Now</NavLink> 
             </div>
-            <div style={secondDiv} className={classes.NavigationItems}>
+            <div className={classes.NavigationItems}>
                 <NavLink to="/days" activeClassName={classes.active}>5 days</NavLink>   
             </div>            
-        </Fragment>             
+        </div>             
     )
 }
 
