569c82344773c7271970616946c5e810df8e3ba4
Resek <matija.resek@gmail.com>
2018-09-19 10:47:33 +0200

    template literals

--- src/components/Days/PartOfDay/PartOfDay.js
+++ src/components/Days/PartOfDay/PartOfDay.js
@@ -4,14 +4,16 @@ import classes from "./PartOfDay.css";
 
 const Day = (props) => {
 
+    console.log(props);
+
     return (
         <div className={classes.Day}>
             <p>{props.partOfDay}</p>
-            <img src={require("../../../assets/weatherIcons/" + props.iconCode + ".png")} alt="weatherIcon" />
+            <img src={require(`../../../assets/weatherIcons/${props.iconCode}.png`)} alt="weatherIcon" />
             <p className={classes.Description}>{props.description}</p>          
             <p>{props.temp}&#8451;</p>            
             <p>{props.wind} km/s</p>
-        </div>      
+        </div>
     )
 }
 
