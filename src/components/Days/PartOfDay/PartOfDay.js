73e5fd92af6636fad4c2b0b003c30ed26865e2bd
Resek <matija.resek@gmail.com>
2018-09-20 10:05:02 +0200

    small weather icons added

--- src/components/Days/PartOfDay/PartOfDay.js
+++ src/components/Days/PartOfDay/PartOfDay.js
@@ -9,7 +9,7 @@ const Day = (props) => {
     return (
         <div className={classes.Day}>
             <p>{props.partOfDay}</p>
-            <img src={require(`../../../assets/weatherIcons/${props.iconCode}.png`)} alt="weatherIcon" />
+            <img src={require(`../../../assets/weatherIcons/${props.iconCode}s.png`)} alt="weatherIcon" />
             <p className={classes.Description}>{props.description}</p>          
             <p>{props.temp}&#8451;</p>            
             <p>{props.wind} km/s</p>
